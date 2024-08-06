045f1cd8a25e504414aae87e6b0258dd4052c67e
오하늘 <dhgksmfdl@naver.com>
2024-08-06 09:41:11 +0900

    8월5일과제추가

--- src/main/java/kr/co/neighbor21/api/controller/HelloWorldController.java
+++ src/main/java/kr/co/neighbor21/api/controller/HelloWorldController.java
@@ -1,2 +1,14 @@
-package kr.co.neighbor21.api.controller;public class HelloWorldController {
+package kr.co.neighbor21.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 컨 알 o  쓸데없는거 지우기
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello-world")
+    public String helloWorld() {
+        // 클린코드 자동정리 컨 알트 l
+        return "Hello World";
+    }
 }
